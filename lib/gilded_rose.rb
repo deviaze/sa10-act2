a6465aefc9173b91b16abf824532aec599fd6ea2
deviaze <dev@deviaze.com>
2024-03-15 03:19:48 -0500

    finished brie tick method time @14:00

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,15 +8,19 @@ class GildedRose
   end
 
   def normal_tick
-    if @quality != 0
-      if @days_remaining > 0
-        @quality -= 1
-      end
-      if @days_remaining <= 0
-        @quality -= 2
-      end
-    end
     @days_remaining -= 1
+    return if @quality == 0
+
+    @quality -= 1
+    @quality -= 1 if @days_remaining <= 0
+  end
+
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0 and @quality < 50
 
   end
 
@@ -26,6 +30,13 @@ class GildedRose
       return normal_tick
     end
     return if name == 'Normal Item'
+
+    case name
+    when 'normal'
+      return normal_tick
+    when 'Aged Brie'
+      return brie_tick
+    end
 
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
